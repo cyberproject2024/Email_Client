682314d54560b6ec3e187f7bc509748e0cf50edb
Anthony F <96542096+julz201@users.noreply.github.com>
2024-08-02 22:03:21 -0500

    Update Code.py
    This is a basic start to writing the code and instructions for installing Kivy.

--- Code.py
+++ Code.py
@@ -1,1 +1,16 @@
 # This is where we will start to write the Python for creating the client. 
+# To install Kivy:
+# 1: Open the terminal in Visual Studio
+# 2: type pip install kivy[full] and hit enter
+
+## imports basic app stuff from Kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class Cyber_Sec_EmailApp(App):
+    def build(self):
+        label = Label(text='Cyber Email')
+        return label
+app = Cyber_Sec_EmailApp()
+app.run()
